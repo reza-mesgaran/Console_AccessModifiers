2f188df87f868f4416a5b11fc5b306b0d189ce3f
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-09 01:24:40 -0400

    Add second namespace for checking accesibility

--- Console_AccessModifiers/Program.cs
+++ Console_AccessModifiers/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Console_AccessModifiers
 {
-    class Program
+    public  class VariablesClass
     {
+        // we have 6 access modifiers
+         public string _Variable_public;          // Accessible from everywhere (Same/Other Assembly)
+         private string _Variable_private;        // Accessible from same class or struct.
+         protected string _Variable_protected;    // Accessible from same class / derived class
+         internal string _Variable_internal;     // Accessible from ONLY same assembly,(not another assembly)
+         protected internal string _Variable_protected_internal; //Accessible from same assembly / drived class in another assembly
+         private protected string _Variable_private_protected;   //Accessible from  the same class (in same Assembly) / drived class (in same Assembly)
+
         static void Main(string[] args)
         {
+            
+        }
+    }
+
+   public class AccessTestClass
+    {
+        public void method()
+        {
+            VariablesClass v1 = new VariablesClass();
+            
+            v1._Variable_public = "Accessible";   //Accessible -  Same Assembly
+            v1._Variable_internal = "Accessible"; //Accessible -  Same Assembly
+            v1._Variable_protected_internal = "Accessible"; //Accessible -  Same Assembly
+            // v1._Variable_private;             Access denied ! (Not Same Class)
+            // v1._Variable_protected            Access denied ! (Not Same Class / Not a Drived Class)
+            // v1._Variable_private_protected;   Access denied !  (Not Same Class/Not a Drived Class)
+        }
+    }
+
+    public class AccessTestClass2: VariablesClass
+    {
+        public void method()
+        {
+            VariablesClass v1 = new VariablesClass();           
         }
     }
 }
---
+++ Console_AccessModifiers2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Console_AccessModifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Console_AccessModifiers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class AccessTestClass_B : VariablesClass
+    {
+        VariablesClass v1 = new VariablesClass();
+
+        AccessTestClass_B b1 = new AccessTestClass_B();
+
+        public void method()
+        {
+            v1._Variable_public= "Accessible";  // Not Same assembly but Is another assembly
+            // v1._Variable_internal             Access denied !  Not Same Assembly
+            // v1._Variable_protected_internal   Access denied !  NOT Derived Class -OR- NOT Same Assembly 
+            // v1._Variable_private;             Access denied !  NOT Same Class
+            // v1._Variable_protected            Access denied !  NOT Derived Class 
+            // v1._Variable_private_protected;   Access denied !  Work only in Same Assemblies
+            
+            b1._Variable_public= "Accessible";     // Not Same assembly but Is another assembly
+            b1._Variable_protected = "Accessible"; // Is Drived Class (even parent is in another assembly)
+            b1._Variable_protected_internal= "Accessible"; // Not Same assembly (internal) -BUT- Drived Class (protected)        
+        }
+    }
+}
